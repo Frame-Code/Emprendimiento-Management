99bd40067bf34579dbf6d646afa024e245e3d425
Rusell <jordquijije@gmail.com>
2025-11-24 21:41:04 -0500

    "AgendaPresentacion,  CargoParticipante, CategoriaPremio y en interfaz tambien ."

---
+++ Datos/Impl/AgendaPresentacionRepositoryImpl.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo;
+using Datos;
+
+namespace Datos
+{
+    internal class AgendaPresentacionRepositoryImpl(AppContext context) : IAgendaPresentacionRepository
+    {
+        public async Task<List<AgendaPresentacion>> ListarAsync() =>
+            await context.AgendaPresentaciones
+                         .Include(a => a.Evento)
+                         .Include(a => a.Emprendimiento)
+                         .ToListAsync();
+
+        public async Task<AgendaPresentacion?> ObtenerAgendaPresentacionAsync(int id) =>
+            await context.AgendaPresentaciones
+                         .Include(a => a.Evento)
+                         .Include(a => a.Emprendimiento)
+                         .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
---
+++ Datos/Impl/CargoParticipanteRepositoryImpl.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo;
+using Datos;
+
+namespace Datos
+{
+    internal class CargoParticipanteRepositoryImpl(AppContext context) : ICargoParticipanteRepository
+    {
+        public async Task<List<CargoParticipante>> ListarAsync() =>
+            await context.CargosParticipante
+                         .ToListAsync();
+
+        public async Task<CargoParticipante?> ObtenerCargoParticipanteAsync(int id) =>
+            await context.CargosParticipante
+                         .FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
