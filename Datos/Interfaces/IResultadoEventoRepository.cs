7f6e73ab3f3b67ce533d6617ff9018db349db40a
Andrei <andreirios2002@gmail.com>
2025-11-25 19:58:05 -0500

    Agenda y Resultado Publicos

--- Datos/Interfaces/IResultadoEventoRepository.cs
+++ Datos/Interfaces/IResultadoEventoRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Datos.Interfaces
 {
-    internal interface IResultadoEventoRepository : IRepository<ResultadoEvento>
+    public interface IResultadoEventoRepository : IRepository<ResultadoEvento>
     {
     }
 }
