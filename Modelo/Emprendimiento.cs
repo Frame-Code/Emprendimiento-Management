57071fe4ad89ec91f3441c5cf86b297edc980714
EdwinMaxDev <edwin789suarezb@gmail.com>
2025-11-26 18:19:16 -0500

    Actualizacion Comentario, Emprendimiento y Evento

--- Modelo/Emprendimiento.cs
+++ Modelo/Emprendimiento.cs
@@ -19,41 +19,6 @@ namespace Modelo
         public int IdRubroEmprendimiento { get; set; }
         public RubroEmprendimiento RubroEmprendimiento { get; set; } = null!;
 
-        // Constructor principal
-        public Emprendimiento(string nombre, string? descripcion, int idFacultad, int idRubro)
-        {
-            Nombre = nombre;
-            Descripcion = descripcion;
-            IdFacultad = idFacultad;
-            IdRubroEmprendimiento = idRubro;
-        }
-
-        // Método 1: Cambiar el logo
-        public void ActualizarLogo(string nuevaRuta)
-        {
-            LogoPath = nuevaRuta;
-        }
-
-        // Método 2: Editar descripción
-        public void EditarDescripcion(string nuevaDescripcion)
-        {
-            Descripcion = nuevaDescripcion;
-        }
-
-        // Método 3: Mostrar información
-        public string MostrarInfo()
-        {
-            return
-                $" Emprendimiento: {Nombre}\n" +
-                $" Facultad: {Facultad?.Nombre}\n" +
-                $" Rubro: {RubroEmprendimiento?.Nombre}\n" +
-                $" Descripción: {Descripcion}";
-        }
-
-        // Método 4: Verificar facultad
-        public bool PerteneceAFacultad(int idFac)
-        {
-            return IdFacultad == idFac;
-        }
+       
     }
 }
