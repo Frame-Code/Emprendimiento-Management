ccd2780610eef15a25901482e99637589d0040ad
TonyHiP <munozanthony535@gmail.com>
2025-11-27 15:37:50 -0500

    Creación de las clases e interfaces RolUsuario, RubroEmprendimiento y Usuario con su implementación.

---
+++ Datos/Impl/RubroEmpendimientoImpl.cs
@@ -0,0 +1,20 @@
+﻿using Datos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Impl
+{
+    internal class RubroEmpendimientoImpl(AppContext context) : IRubroEmprendimiento
+    {
+        public async Task<List<RubroEmprendimiento>> ListarAsync() =>
+            await context.RubrosEmprendimiento.ToListAsync();
+
+        public async Task<RubroEmprendimiento?> ObtenerPorIdAsync(int id) =>
+            await context.RubrosEmprendimiento.FirstOrDefaultAsync(rp => rp.Id == id);
+    }
+}
---
+++ Datos/Impl/UsuarioImpl.cs
@@ -0,0 +1,24 @@
+﻿using Datos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Impl
+{
+    internal class UsuarioImpl(AppContext context) : IUsuario
+    {
+        public async Task<List<Usuario>> ListarAsync() =>
+            await context.Usuarios.
+            Include(u => u.RolUsuario)
+            .ToListAsync();
+
+        public async Task<Usuario?> ObtenerPorIdAsync(int id) =>
+            await context.Usuarios
+            .Include(u => u.RolUsuario)
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+}
