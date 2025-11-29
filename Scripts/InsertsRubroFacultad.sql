3f56f5dac9738036920ee990d685eabbffc98b0d
Daniel Mora C. <moraisurcan@hotmail.com>
2025-11-29 02:00:50 -0500

    feat: script to add rubro and facultad

---
+++ Scripts/InsertsRubroFacultad.sql
@@ -0,0 +1,9 @@
+
+insert into RubrosEmprendimiento values
+('Tecnologia', 'Tecnologia'),
+('Gastronomia', 'Gastronomia')
+
+insert into Facultades values
+('Ciencias matematicas y fisicas', 'Ciencias matematicas y fisicas'),
+('Medicina', 'Medicina')
+
