af5cca399c4bd7b3926e53d59ce6b4512c14dc63
Rusell <jordquijije@gmail.com>
2025-12-14 23:08:35 -0500

    Agregar foto y mostrarla y que aparezca en mis archivos

--- WinForms/Views/RegistroParticipantesView.cs
+++ WinForms/Views/RegistroParticipantesView.cs
@@ -12,10 +12,13 @@ namespace WinForms.Views
     public partial class RegistroParticipantesView : Form
     {
         private readonly RegistroParticipanteController _miControlador;
-        public RegistroParticipantesView(RegistroParticipanteController controller)
+        private readonly FileController _fileController;
+        private List<string> filesPath = new List<string>();
+        public RegistroParticipantesView(RegistroParticipanteController controller, FileController fileController)
         {
             _miControlador = controller;
             InitializeComponent();
+            _fileController = fileController;
         }
 
         private async void RegistroParticipantesView_Load(object sender, EventArgs e)
@@ -71,12 +74,55 @@ namespace WinForms.Views
             {
                 MessageBox.Show("¡Guardado correctamente!");
                 this.DialogResult = DialogResult.OK;
-                this.Close();                       
+                this.Close();
             }
             else
             {
                 MessageBox.Show("Error: " + respuesta.Message);
             }
+        }
+
+        private void btnFoto_Click(object sender, EventArgs e)
+        {
+            using var ofd = new OpenFileDialog
+            {
+                Title = "Selecciona una imagen",
+                Filter = "Imágenes|*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.webp",
+                Multiselect = true
+            };
+
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            pnlFoto.Controls.Clear();
+            var flowLayout = new FlowLayoutPanel();
+            flowLayout.FlowDirection = FlowDirection.LeftToRight;
+            flowLayout.WrapContents = true;
+            flowLayout.AutoScroll = true;
+            flowLayout.Dock = DockStyle.Fill;
+
+            pnlFoto.Controls.Add(flowLayout);
+            var listPath = ofd.FileNames;
+
+            foreach (var file in ofd.FileNames)
+            {
+                var pb = new PictureBox
+                {
+                    Width = 120,
+                    Height = 120,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Margin = new Padding(6),
+                    BorderStyle = BorderStyle.FixedSingle
+                };
+
+                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+                using var imgTemp = Image.FromStream(fs);
+                pb.Image = new Bitmap(imgTemp);
+                pb.Tag = file;
+                filesPath.Add(file);
+                flowLayout.Controls.Add(pb);
+            }
+            flowLayout.ResumeLayout();
         }
     }
 }
