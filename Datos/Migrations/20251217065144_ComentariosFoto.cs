6050a2f8062d4d4670812724bcda6a7866e73de8
Rusell <jordquijije@gmail.com>
2025-12-17 22:27:02 -0500

    Agrega entidad ComentarioFoto, repositorio y migración

---
+++ Datos/Migrations/20251217065144_ComentariosFoto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComentariosFoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ComentarioFoto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Texto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FotoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComentarioFoto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ComentarioFoto_Fotos_FotoId",
+                        column: x => x.FotoId,
+                        principalTable: "Fotos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComentarioFoto_FotoId",
+                table: "ComentarioFoto",
+                column: "FotoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComentarioFoto");
+        }
+    }
+}
